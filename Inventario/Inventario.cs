770d734b585dd328cdb71a042e798fea0fbd6e49
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-07-24 19:49:14 +0200

    Add files via upload

---
+++ Inventario/Inventario.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Inventario : MonoBehaviour {
+    public Inventario it2;
+    public string dato;
+    public string dato1;
+    public int dato2;
+    public string nombre;
+    public string img;
+    public int id;
+    public bool guardar = false;
+    List<Inventario> Invs;
+
+
+    public Inventario(string nombre2, string imagen2, int id2)
+    {
+        nombre = nombre2;
+        img = imagen2;
+        id = id2;
+    }
+
+    void Awake()
+    {
+        Invs = new List<Inventario>();
+    }
+   
+    void Update()
+    {
+        instacia(guardar);
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ver();
+            
+
+        }
+       
+    }
+   
+        public void instacia(bool guardar)
+        {
+            if (guardar)
+            {
+                it2 = new Inventario(dato, dato1, dato2);
+                Invs.Add(it2);
+            
+            }
+           
+
+
+        }
+        public void ver()
+        {
+            foreach (Inventario x in Invs)
+            {
+           Debug.Log(x.nombre);
+           
+            }
+        }
+    
+}
---
+++ Inventario/MetodosItem.cs
@@ -0,0 +1,122 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+
+public class MetodosItem : MonoBehaviour
+{
+    public Collider colider;
+    public string mensaje = "Pulsa E para cogerlo";
+    public bool activo = false;
+    public GUIStyle font;
+    public bool destruido = false;
+    public bool recogido = false;
+    public GameObject aux;
+    private Inventario inv;
+     public string nombre;
+    public string img;
+    public int id;
+
+
+    void Awake()
+    {
+
+        colider = GetComponent<Collider>();
+        aux = GameObject.FindGameObjectWithTag("Player");
+        inv =aux.GetComponent<Inventario>();
+      
+
+
+
+
+
+}
+    void Update()
+    {
+  
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            colider.isTrigger = true;
+           
+
+
+
+
+
+
+        }
+        else
+        {
+            colider.isTrigger = false;
+
+        }
+
+
+    }
+    void OnGUI()
+    {
+
+        if (activo)
+        {
+
+
+
+            GUI.Label(new Rect(Screen.width / 2 - 100, 50, 200, 30), mensaje, font);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            recogido = true;
+            inv.guardar = recogido;
+            inv.dato = nombre;
+            inv.dato1 = img;
+            inv.dato2 = id;
+            Destroy(gameObject);
+
+        }
+      
+
+
+    }
+
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            activo = true;
+        }
+
+
+
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            activo = false;
+        }
+    }
+    
+
+}
+ 
+    
+
+    
+    
+        
+
+    
+    
+
+
+
